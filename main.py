ae1cd18250d0d33a614886c8eecb0af1746d8756
Supergroovy17 <166076637+Supergroovy17@users.noreply.github.com>
2024-04-29 22:15:57 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+     
+import moody
+mood=input("How are you feeling today? ")
+moody.mood_response(mood)
---
+++ moody.py
@@ -0,0 +1,12 @@
+
+def mood_response(moody):
+    if moody == ('happy'):
+        print('Dont lie !')
+    elif moody==('sad'):
+        print('shut up')
+    elif moody == ('excited'):
+        print('simmer down')
+    elif moody == ('exhausted'):
+        print('get back too work ya lazy pos')
+    else:
+        print('couldve just said happy but you wanted to be difficult \b Not like I had anything better to do guess we will try and figure this out next time so please choose an option like happy sad excited or exhausted ')
